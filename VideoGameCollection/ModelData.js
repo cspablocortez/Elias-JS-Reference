dda7030e29c77fbab85366ee146426ad710d1ece
Pablo Cortez <cspablocortez@gmail.com>
2023-12-13 20:58:48 -0500

    Update ModelData.js

--- VideoGameCollection/ModelData.js
+++ VideoGameCollection/ModelData.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 async function fetchVideoGameData() { // this is  a promise
-    const requestURL = "https://gist.githubusercontent.com/cspablocortez/3e2f51acd357c183c013f32a65215ef6/raw/d92a6054ac7fa14c023b1c116fdd92b0653c76bf/videogameData.json";
+    const requestURL = "https://gist.githubusercontent.com/cspablocortez/3e2f51acd357c183c013f32a65215ef6/raw/6791958c5bd8c3dc787b1e2aa14bc35043ddcfcd/videogameData.json";
     const request = new Request(requestURL);
     const response = await fetch(request);
     const videoGames = await response.json();
