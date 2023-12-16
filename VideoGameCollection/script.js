2df5cb58ec48614d9f2ca7eaf6818daf8d56b94c
Pablo Cortez <cspablocortez@gmail.com>
2023-12-15 19:44:39 -0500

    Refactor JavaScript

--- VideoGameCollection/script.js
+++ VideoGameCollection/script.js
@@ -1,15 +1,58 @@
+let videogameData;
+let currentId;
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchVideoGameData()
+
+    // Pause/Play functionality
+    var playButton = document.querySelector('.control-play');
+    var album = document.querySelector('.album');
+
+    playButton.addEventListener('click', function() {
+        var musicPlayerContainer = document.querySelector('.music-player-container');
+        musicPlayerContainer.classList.toggle('is-playing');
+    });
+
+});
+
+async function fetchVideoGameData() { // this is  a promise
+    const requestURL = "https://gist.githubusercontent.com/cspablocortez/3e2f51acd357c183c013f32a65215ef6/raw/df0f219ce02cb71b8aa5729138028e9a6574d91f/videogameData.json";
+    const request = new Request(requestURL);
+    const response = await fetch(request);
+    const videoGames = await response.json();
+    console.log("Data fetched.");
+    videogameData = videoGames;
+    populateCover(videogameData);
+}
+
+function populateCover(obj, id) {
+    const imageCover = document.querySelector(".album-art");
+    const title = document.querySelector(".album-title");
+    const publisher = document.querySelector(".artist-name");
+    const year = document.querySelector(".song-title");
+
+    if (id == null) {
+        currentId = 0;
+    }
+
+    title.textContent = `${obj.games[currentId].name}`;
+    publisher.textContent = `${obj.games[currentId].publisher}`;
+    year.textContent = `${obj.games[currentId].year}`;
+    imageCover.style.backgroundImage = `url(${obj.games[currentId].imageURL})`;
+}
 
 
+function nextItem() {
+    // move catalog forward
+    currentId = Math.abs((currentId + 1) % videogameData.games.length);
+    populateCover(videogameData, currentId);
+    console.log(currentId);
+}
 
-const imageCoverURLs =
-    [
-        "https://i.pinimg.com/736x/73/32/52/7332527b2baa4785959b1b8e2ba41dce.jpg",
-        "https://logodix.com/logo/45400.jpg",
-        "https://steamuserimages-a.akamaihd.net/ugc/793135280575664837/0BBE59C999EBF567C86CB78D0452F243C9E389FD/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-        "https://picsum.photos/200",
-        "https://picsum.photos/200"
-    ]
-
-const imageCover = document.querySelector('.album-art');
-let idx = 0;
+function prevItem() {
+    // move catalog forward
+    currentId = Math.abs((currentId - 1)) % videogameData.games.length;
+    populateCover(videogameData, currentId);
+    console.log(currentId);
+}
 
