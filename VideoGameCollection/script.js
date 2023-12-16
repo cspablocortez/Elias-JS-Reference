44ffdbdd87320606cf0afaba7aa66a1950231d79
Pablo Cortez <cspablocortez@gmail.com>
2023-12-15 20:17:52 -0500

    Data updates

--- VideoGameCollection/script.js
+++ VideoGameCollection/script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 async function fetchVideoGameData() { // this is  a promise
-    const requestURL = "https://gist.githubusercontent.com/cspablocortez/3e2f51acd357c183c013f32a65215ef6/raw/df0f219ce02cb71b8aa5729138028e9a6574d91f/videogameData.json";
+    const requestURL = "./data.json";
     const request = new Request(requestURL);
     const response = await fetch(request);
     const videoGames = await response.json();
