c16892724c02cd7c20ff8067636e614a8a1ab853
Pablo Cortez <cspablocortez@gmail.com>
2024-01-13 15:59:56 -0800

    Update script.js

--- EliasExercise/script.js
+++ EliasExercise/script.js
@@ -4,3 +4,12 @@ headerElement.textContent = "A good title";
 const paragraphElement = document.getElementById('paragraphElement');
 paragraphElement.textContent = "JavaScript is a programming language and core technology of the World Wide Web, alongside HTML and CSS. As of 2023, 98.7% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on users' devices.";
 
+const myDiv = document.createElement('div');
+
+myDiv.style.border = '4px solid red';
+myDiv.style.height = '100px';
+myDiv.style.width  = '100px';
+myDiv.style.backgroundColor = 'green';
+myDiv.style.borderRadius = '15px';
+
+document.body.appendChild(myDiv);
