8a76c32609590014099749783df8c728d404414f
Pablo Cortez <cspablocortez@gmail.com>
2024-01-24 17:41:23 -0800

    Formatting tool

--- Formatting/script.js
+++
@@ -1,7 +0,0 @@
-
-function format(e) {
-    const inputElement = document.getElementById(e.id);
-    const formattedNumberElement = document.getElementById('formatted-phone-number');
-    const num = inputElement.value;
-    formattedNumberElement.textContent = `(${num.substring(0, 3)})-${num.substring(4, 7)}-${num.substring(8, 12)}`;
-}
