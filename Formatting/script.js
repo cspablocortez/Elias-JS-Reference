3b72ac074486fccd1c8ddca0697ba7e5a67944da
Pablo Cortez <cspablocortez@gmail.com>
2024-01-26 16:49:53 -0800

    Update script.js

--- Formatting/script.js
+++ Formatting/script.js
@@ -1,1 +1,10 @@
-const 
+const formatNumber = (number) => {
+    const phoneOutput = document.getElementById("phone-output");
+
+    const areaCode   = number.slice(0, 3);
+    const middlePart = number.slice(3, 6);
+    const lastPart   = number.slice(6, 9);
+
+    const formattedNumber = number.length === 10 ? `(${areaCode})-${middlePart}-${lastPart}` : "Invalid input";
+    phoneOutput.textContent = formattedNumber;
+}
