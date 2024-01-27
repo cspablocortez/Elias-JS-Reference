3aa9fbcdab53d36ad1925d912e4aa3cc0c7b50de
Pablo Cortez <cspablocortez@gmail.com>
2024-01-26 18:00:00 -0800

    MLA Formatting

--- Formatting/script.js
+++ Formatting/script.js
@@ -11,3 +11,38 @@ const formatNumber = () => {
     const phoneOutput = document.getElementById("phone-output");
     phoneOutput.textContent = formattedNumber;
 }
+
+const formatCitation = () => {
+    const mlaInput = document.getElementById("mla-input");
+
+    const citation = mlaInput.value.split(".");
+
+    console.log(citation);
+
+    let formattedCitation = {
+        "author": "",
+        "sourceTitle": "",
+        "containerTitle": "",
+        "otherContributors": [],
+        "version": "",
+        "number": "",
+        "publisher": "",
+        "publicationDate": "",
+        "location": "",
+        "url": ""
+    };
+
+    reverseAuthor = citation[0].split(",");
+    formattedCitation.author = reverseAuthor[1] + " " + reverseAuthor[0];
+    formattedCitation.sourceTitle = citation[1];
+    
+    console.log(formattedCitation.author);
+    console.log(formattedCitation.sourceTitle.replace(/\s+/g, ''));
+
+    // Return all the values
+
+    // traverse object?
+
+    const mlaOutput = document.getElementById("mla-output");
+
+}
