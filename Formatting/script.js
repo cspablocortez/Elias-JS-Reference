227c688f4c4c6fb723e558a82a6bf90347e68b85
Pablo Cortez <cspablocortez@gmail.com>
2024-01-26 16:59:54 -0800

    Number Formatting

--- Formatting/script.js
+++ Formatting/script.js
@@ -1,10 +1,13 @@
 const formatNumber = () => {
-    const phoneOutput = document.getElementById("phone-output");
-
+    const phoneInput = document.getElementById("phone-input");
+    const number = phoneInput.value;
+    
     const areaCode   = number.slice(0, 3);
     const middlePart = number.slice(3, 6);
-    const lastPart   = number.slice(6, 9);
-
-    const formattedNumber = number.length === 10 ? `(${areaCode})-${middlePart}-${lastPart}` : "Invalid input";
+    const lastPart   = number.slice(6, 10);
+    
+    const formattedNumber = number.length === 10 ? `(${areaCode})-${middlePart}-${lastPart}` : "";
+    
+    const phoneOutput = document.getElementById("phone-output");
     phoneOutput.textContent = formattedNumber;
 }
